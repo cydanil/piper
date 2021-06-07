57a26c705cbf58b1d6124c5cc6e87c19f3c3d76a
Cyril Danilevski <cydanil@gmail.com>
2021-06-07 14:44:51 +0200

    Add 3 synchronized images output

--- src/piper/Piper.py
+++ src/piper/Piper.py
@@ -5,15 +5,20 @@
 #############################################################################
 import threading
 import time
+from pathlib import Path
 
 import numpy as np
-
-from pathlib import Path
-from karabo.bound import (PythonDevice, INPUT_CHANNEL, KARABO_CLASSINFO,
-                          SLOT_ELEMENT, OUTPUT_CHANNEL, Schema, State, Hash,
-                          Epochstamp, Trainstamp, Timestamp, STRING_ELEMENT)
+from cycler import cycler
+from karabo.bound import (DOUBLE_ELEMENT, IMAGEDATA_ELEMENT, INPUT_CHANNEL,
+                          KARABO_CLASSINFO, NODE_ELEMENT, OUTPUT_CHANNEL,
+                          SLOT_ELEMENT, STRING_ELEMENT, DaqDataType,
+                          Epochstamp, Hash, ImageData, PythonDevice, Schema,
+                          State, Timestamp, Trainstamp, Types, Unit)
+from processing_utils.rate_calculator import RateCalculator
 
 from ._version import version as deviceVersion
+
+PIXELS = 512
 
 
 @KARABO_CLASSINFO("Piper", deviceVersion)
@@ -36,34 +41,67 @@ class Piper(PythonDevice):
 
             STRING_ELEMENT(expected).key('saveTo')
             .assignmentOptional()
-            .defaultValue('/gpfs/exfel/data/scratch/danilevc/lpd_data_to_validate/Q2M1_cell_8_relgain_only')
+            .defaultValue('/gpfs/exfel/data/scratch/danilevc/lpd_data_to_validate/Q2M1_cell_8_relgain_only')  # noqa
             .reconfigurable()
             .commit(),
         )
 
+        triple_output_schema = Schema()
+        (
+            NODE_ELEMENT(triple_output_schema).key('images')
+            .displayedName('images')
+            .setDaqDataType(DaqDataType.TRAIN)
+            .commit(),
+
+            IMAGEDATA_ELEMENT(triple_output_schema).key('images.raw')
+            .displayedName('Raw')
+            .setDimensions([PIXELS, PIXELS])
+            .setType(Types.UINT32)
+            .commit(),
+
+            IMAGEDATA_ELEMENT(triple_output_schema).key('images.gain')
+            .displayedName('Gain')
+            .setDimensions([PIXELS, PIXELS])
+            .setType(Types.UINT8)
+            .commit(),
+
+            IMAGEDATA_ELEMENT(triple_output_schema).key('images.corrected')
+            .displayedName('Corrected')
+            .setDimensions([PIXELS, PIXELS])
+            .setType(Types.FLOAT)
+            .commit(),
+
+            SLOT_ELEMENT(expected).key('spit3Images')
+            .displayedName('Send 3 images')
+            .commit(),
+
+            OUTPUT_CHANNEL(expected).key('tripleImages')
+            .displayedName('3 Images out')
+            .dataSchema(triple_output_schema)
+            .commit(),
+
+            DOUBLE_ELEMENT(expected).key('frameRate')
+            .displayedName('3 Images rate')
+            .unit(Unit.HERTZ)
+            .readOnly().initialValue(0.)
+            .commit()
+
+        )
+
     def __init__(self, configuration):
-        # always call PythonDevice constructor first!
         super().__init__(configuration)
-        # Define the first function to be called after the constructor has
-        # finished
         self.registerInitialFunction(self.initialization)
-        t = threading.Thread(target=self.serve)
-        t.start()
+        self.thread_serve = threading.Thread(target=self.serve)
+        self.thread_serve.start()
+
+        self.thread_3out = threading.Thread(target=self.serve_triple_output)
+        self.thread_3out.start()
+
         self.saved_count = 0
 
     def initialization(self):
-        """This method will be called after the constructor.
-
-        If you need methods that can be callable from another device or GUI
-        you may register them here:
-        self.KARABO_SLOT(self.myslot1)
-        self.KARABO_SLOT(self.myslot2)
-        ...
-        Corresponding methods (myslot1, myslot2, ...) should be defined in this
-        class
-        """
-        # Define your slots here
         self.KARABO_SLOT(self.thingy)
+        self.KARABO_SLOT(self.spit3Images)
         self.updateState(State.NORMAL)
         self.KARABO_ON_DATA("input", self.onData)
 
@@ -103,3 +141,43 @@ class Piper(PythonDevice):
         except Exception:
             print(data.keys(), data)
         self.saved_count += 1
+
+    def spit3Images(self):
+        state = State.PROCESSING if self['state'] == State.NORMAL else State.NORMAL  # noqa
+        self.updateState(state)
+
+    def serve_triple_output(self):
+        acq_rate = RateCalculator(refresh_interval=1)
+        raw = [np.array([100] * PIXELS * PIXELS, dtype=np.uint32).reshape([PIXELS, PIXELS]),  # noqa
+               np.array([200] * PIXELS * PIXELS, dtype=np.uint32).reshape([PIXELS, PIXELS]),  # noqa
+               np.array([300] * PIXELS * PIXELS, dtype=np.uint32).reshape([PIXELS, PIXELS])]  # noqa
+        gain = [np.zeros([PIXELS, PIXELS], dtype=np.uint8),
+                np.ones([PIXELS, PIXELS], dtype=np.uint8),
+                np.array([2] * PIXELS * PIXELS, dtype=np.uint8).reshape([PIXELS, PIXELS])]  # noqa
+        corrected = [np.array([50] * PIXELS * PIXELS, dtype=np.float32).reshape([PIXELS, PIXELS]),  # noqa
+                     np.array([150] * PIXELS * PIXELS, dtype=np.float32).reshape([PIXELS, PIXELS]),  # noqa
+                     np.array([250] * PIXELS * PIXELS, dtype=np.float32).reshape([PIXELS, PIXELS])]  # noqa
+        cycles = cycler(raw=raw, gain=gain, corrected=corrected)()
+
+        h = Hash()
+        while True:
+            if self['state'] == State.PROCESSING:
+                data = next(cycles)
+                h['images.raw'] = ImageData(data['raw'])
+                h['images.gain'] = ImageData(data['gain'])
+                h['images.corrected'] = ImageData(data['corrected'])
+                self.log.INFO(f'{data}\n{h}')
+                self.writeChannel('tripleImages', h)
+
+                acq_rate.update()
+                rate = acq_rate.refresh()
+                if rate is not None:
+                    self['frameRate'] = rate
+            time.sleep(0.1)
+
+    def __del__(self):
+        """Stop the threads."""
+        self.thread_serve.done = True
+        self.thread_serve.join()
+        self.thread_3out.done = True
+        self.thread_3out.join()
